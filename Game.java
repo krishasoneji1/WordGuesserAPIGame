9f5d4101e27a6b65bf80fef7d3f7370c4e643a83
krishasoneji1 <soneji0012@mydusd.org>
2024-08-08 19:42:01 -0700

    added all functionalities

--- Game.java
+++ Game.java
@@ -1,17 +1,22 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game {
     private int guesses=5;
+    private int count = 0;
     private String randomWord;
     private String [] gameBoard = {"_", "_", "_", "_", "_"};
     private String [] userGuess = {"_", "_", "_", "_", "_"};
-    private String [] randomWordArray = {"_", "_", "_", "_", "_", "_", "_", "_", "_"};
-    
+    private String [] randomWordArray = {"_", "_", "_", "_", "_"};
+    private ArrayList<String> outputArray = new ArrayList<String>();
+    private ArrayList<String> letters = new ArrayList<String>();
+    private String [][] guessesArray = {{""}, {""}, {""}, {""}, {""}};
 
     public Game() throws IOException, InterruptedException{
         try {
             randomWord = Game.generateRandomWord();
+            wordToStringArray (randomWord);
         }
         catch (Exception e){
             randomWord = "slope";
@@ -19,8 +24,10 @@ public class Game {
 
     }
     public void run() throws IOException, InterruptedException {
-        makeBoard(gameBoard);
+        System.out.println("Welcome to the Wordle console based game! Guess the 5 letter word!");
+        makeBoard();
         Scanner scan = new Scanner(System.in); 
+        System.out.println("You have "+ guesses + " guesses");
         System.out.println("Guess a word");
         String userInput = scan.nextLine();
         System.out.println();
@@ -29,9 +36,13 @@ public class Game {
             guesses-=1;
             toStringArray(userInput);
             giveHint();
+            addOutput(userInput);
+            currentGame(userInput);
+            makeBoard();
             
         }
-        System.out.println("You have "+ guesses + " guesses");
+        System.out.println("--------------");
+        System.out.println("Guesses left: "+ guesses);
         System.out.println();
         
         while (checkGuess(userInput)) {
@@ -40,28 +51,35 @@ public class Game {
             if (checkValidity(userInput)){
                 toStringArray(userInput);
                 giveHint();
+                System.out.println();
+                addOutput(userInput);
+                System.out.println();
+                currentGame(userInput);
+                makeBoard();
                 guesses-=1;
             }
-            System.out.println("You have "+ guesses + " guesses");
+            System.out.println("--------------");
+            System.out.println("Guesses left: "+ guesses);
             System.out.println();
-            
         }
         scan.close();
 
     }
 
-    public void makeBoard(String[] gameBoard){
-        for (String x: gameBoard){
-            System.out.print(x + " ");
-            
+    public void makeBoard(){
+        for (String [] x: guessesArray){
+            for (String y: x){
+                System.out.print(y + " ");
+            }
+            System.out.println();
         }
-        System.out.println();
-        System.out.println();
     }
-
+        
+    
     public static String generateRandomWord() throws IOException, InterruptedException{
         Request request = new Request();
         String result = request.find();
+        //
         System.out.println(result);
         return result.substring(2, 7);
     }
@@ -81,16 +99,22 @@ public class Game {
     }
 
     public void giveHint() {
-        System.out.println("x");
-        for (String s: userGuess) {
-            for (int i=2; i<7; i++) {
-                if (s == randomWordArray[i]) {
-                    System.out.println ("Hint:" + s + " is a letter in the word, but it is in the wrong spot.");
+        for (int i=0; i<5; i++) {
+            for (String s: userGuess) {
+                if (!letters.contains(s)){
+                    if (s.equals(randomWordArray[i]) && !randomWordArray[i].equals(userGuess[i])) {
+                        letters.add(s);
+                    } 
                 }
+                            
+            }
         }
-        
-            
+        System.out.println("Current Hints:");
+        for (String l: letters) {
+            System.out.println( l +" is a letter in the word, but it is in the wrong spot.");
         }
+                    
+                    
     }
 
     public void toStringArray(String input) {
@@ -98,6 +122,7 @@ public class Game {
             char character = input.charAt(i);
             String characterStr = Character.toString(character);
             userGuess[i] = characterStr;
+
         }
     }
 
@@ -107,6 +132,7 @@ public class Game {
             String characterStr = Character.toString(character);
             randomWordArray[i] = characterStr;
         }
+        
     }
     
     public boolean checkValidity(String userInput) {
@@ -128,7 +154,27 @@ public class Game {
         return true;
     }
 
-    
+    public void currentGame (String userInput) {
+        System.out.println("Game Board:");
+        for (int i=0; i<5; i++) {
+            for (String s: userGuess) {
+                if (s.equals(randomWordArray[i]) && randomWordArray[i].equals(userGuess[i])) {
+                    gameBoard[i] = s;
+                }
+            }
+        }
+        guessesArray[count] = gameBoard;    
+        gameBoard = new String[] {"_", "_", "_", "_", "_"};
+        count++;
+    }
 
-
+    public void addOutput (String userInput) {
+        outputArray.add(userInput);
+        System.out.println();
+        System.out.println("Here are your previous guesses:");
+        for (String output:outputArray) {
+            System.out.println(output);
+        }
+        System.out.println();
+    }
 }
